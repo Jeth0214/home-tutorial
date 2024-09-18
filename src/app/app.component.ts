2d015f3c6a4781dede31c927f0c063cf027baa23
Jeth0214 <rolandjethrosuyom@yahoo.com>
2024-09-18 21:56:53 +0300

    migrating project to version 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'home-tutorial';
+  title = 'homes';
 }
